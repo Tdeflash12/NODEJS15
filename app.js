d14a7ddd52b980c1f37922a9fd742d432410892d
Abhesh Mandal <abheshmandal249@gmail.com>
2024-09-01 23:40:24 +0545

    CRUD DONE

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const { blogs, sequelize } = require('./model/index')
 // const multer = require('./middleware/multerConfig').multer
 // const storage = require('./middleware/multerConfig').storage
-const {multer,storage,storage2,storage3} = require('./middleware/multerconfig')
+const {multer,storage,storage2,storage3} = require('./middleware/multerConfig')
 const upload = multer({storage:storage})
 
 
@@ -16,6 +16,30 @@ app.set('view engine','ejs')
 require("./model/index")
 app.use(express.urlencoded({extended : true}))
 
+app.get("/",async (req,res)=>{
+   const datas = await blogs.findAll() // select * from blogs returns array 
+  
+   res.render("home",{blogs : datas})
+})
+
+app.get("/blog/:id",async (req,res)=>{
+    const id = req.params.id
+    const blog =  await blogs.findByPk(id) // returns object 
+
+    res.render("singleBlog.ejs",{blog : blog})
+})
+
+app.get("/delete/:id",async (req,res)=>{
+    const id = req.params.id
+    await blogs.destroy({
+        where : {
+            id : id
+        }
+    })
+    res.redirect("/")
+})
+
+
 app.get("/create",(req,res)=>{
     res.render("create")
 
@@ -26,11 +50,15 @@ app.post('/create',upload.single('image') ,async (req,res)=>{
     // const title = req.body.title 
     // const subtitle = req.body.subtitle 
     // const description = req.body.description
+ 
+   const filename = req.file.filename
     const {title,subtitle,description} = req.body 
    await blogs.create({
         title : title,
         subtitle : subtitle, 
-        description : description
+        description : description, 
+        image : filename
+       
     })
     res.send("Blog added successfully")
 
@@ -38,6 +66,7 @@ app.post('/create',upload.single('image') ,async (req,res)=>{
 
 
 app.use(express.static('public/css/'))
+app.use(express.static("./storage/"))
 
 app.listen(3000,()=>{
     console.log("project suru vayo hai tw nodejs ko")
